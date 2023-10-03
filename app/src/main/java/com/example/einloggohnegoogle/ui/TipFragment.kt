9a4c0518e6851143b4fe965d4499d00c1339e59d
Andreas Pelczer <134264285+AndreasPelczer@users.noreply.github.com>
2023-10-03 19:40:33 +0200

    wird langsam, Video fertig, Tip, neues,detail,  speichern geht plötzlich nicht mehr.

--- app/src/main/java/com/example/einloggohnegoogle/ui/TipFragment.kt
+++ app/src/main/java/com/example/einloggohnegoogle/ui/TipFragment.kt
@@ -1,5 +1,6 @@
 package com.example.einloggohnegoogle.ui
 
+import android.net.Uri
 import com.example.einloggohnegoogle.adapter.TipAdapter
 import android.os.Bundle
 import android.util.Log
@@ -7,6 +8,7 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.ImageView
+import android.widget.VideoView
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -25,9 +27,26 @@ class TipFragment : Fragment() {
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View {
+
         viewModel.loadData()
+        val videoView3 = VideoView(requireContext())
+        // Lade das Video
+        val videoUri = Uri.parse("https://youtu.be/0IxQ16bPzak?si=vcGyShf4njT0hkW6")
+        videoView3.setVideoURI(videoUri)
+        // Starte das Video
+        videoView3.start()
+
+        // Gib das VideoView-Objekt zurück
         binding = FragmentTipBinding.inflate(inflater, container, false)
         return binding.root
+        // Erstelle das VideoView-Objekt
+    }
+    private fun extractVideoId(youtubeUrl: String): String {
+        val pattern =
+            "(?<=watch\\?v=|/videos/|embed/|youtu.be/|/v/|/e/|watch\\?v%3D|watch\\?feature=player_embedded&v=|%2Fvideos%2F|embed%\u200C\u200B2Fvideos\u200C\u200B|youtu.be%2F)[^#&?\\n]*"
+        val compiledPattern = Regex(pattern)
+        val matcher = compiledPattern.find(youtubeUrl)
+        return matcher?.value ?: ""
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
