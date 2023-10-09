0d1432316eee5d61aa647514cc8701e7933c7f2e
Andreas Pelczer <134264285+AndreasPelczer@users.noreply.github.com>
2023-10-10 00:09:14 +0200

    wird langsam, Video fertig, Tip, neues,detail, rv funtionieren,infoseiten,speichern geht

--- app/src/main/java/com/example/einloggohnegoogle/ui/RezeptBearbeitenFragment.kt
+++ app/src/main/java/com/example/einloggohnegoogle/ui/RezeptBearbeitenFragment.kt
@@ -5,6 +5,7 @@ import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.fragment.app.Fragment
 import androidx.fragment.app.viewModels
 import androidx.navigation.fragment.findNavController
@@ -53,21 +54,36 @@ class RezeptBearbeitenFragment : Fragment() {
 
         binding.RezeptSpeichernBTN.setOnClickListener {
             Log.e("Buttonrezepspeichern Button gespeichert", "Rezept gespeichert")
+            val currentUserID = id
+// ErstellerId = RezeptId.....ich habe die Rezept id, die currrentUserId, den erstellernamen
+
 
             val updatedName = binding.editRezeptNameTV.text.toString()
             val updatedZutaten = binding.editRezeptZutatenTV.text.toString()
             val updatedZubereitung = binding.editRezeptZubereitungTV.text.toString()
-                // beitrag aktualisieren.//
+            // beitrag aktualisieren.//
             if (id != null) {
-                viewModel.updateRezeptDetail(id!!, updatedName, updatedZutaten, updatedZubereitung)
+                viewModel.updateRezeptDetail(
+                    id!!,
+                    updatedName,
+                    updatedZutaten,
+                    updatedZubereitung
+                )
+                Toast.makeText(
+                    requireContext(),
+                    "Rezept erfolgreich bearbeitet!",
+                    Toast.LENGTH_SHORT
+                ).show()
+
                 val navController = findNavController()
                 navController.navigate(RezeptBearbeitenFragmentDirections.actionRezeptBearbeitenFragmentToDataFragment())
             }
-
-
         }
 
+
     }
+
 }
+
 
 
